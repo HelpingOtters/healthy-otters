b51a6c8a78b5feaa28cbc609284d72815a79779f
thedandano <dandano2006@gmail.com>
2020-02-25 21:19:15 -0800

    added empty string catcher

--- src/dw/MonthlyReport.java
+++ src/dw/MonthlyReport.java
@@ -28,60 +28,72 @@ public class MonthlyReport extends HttpServlet {
 
 
 		String sql =  "SELECT dg.trade_name, SUM(quantity) AS \"sold\"\r\n" + 
-					  "from prescription pres join drug dg\r\n" + 
-				      "	on pres.drug_id = dg.drug_id\r\n" + 
-				      "WHERE pres.date BETWEEN DATE_SUB(curdate(), INTERVAL 1 MONTH) AND curdate()\r\n" + 
-				      "AND pharmacy_id = ?\r\n" + 
-				      "group by dg.trade_name;";
+				"from prescription pres join drug dg\r\n" + 
+				"	on pres.drug_id = dg.drug_id\r\n" + 
+				"WHERE pres.date BETWEEN DATE_SUB(curdate(), INTERVAL 1 MONTH) AND curdate()\r\n" + 
+				"AND pharmacy_id = ?\r\n" + 
+				"group by dg.trade_name;";
 
 		response.setContentType("text/html"); // Set response content type
 		PrintWriter out = response.getWriter();
 
 		String pharmacy_id = request.getParameter("pharmacy_id");
 
-		try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS)) {
+		if(pharmacy_id != "") {
+			try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS)) {
 
-			//Prepare Select
-			PreparedStatement pstmt = conn.prepareStatement(sql);
-			pstmt.setString(1, pharmacy_id);
-			ResultSet rs = pstmt.executeQuery();
+				//Prepare Select
+				PreparedStatement pstmt = conn.prepareStatement(sql);
+				pstmt.setString(1, pharmacy_id);
+				ResultSet rs = pstmt.executeQuery();
 
-			//Start html output
+				//Start html output
+				out.println("<!DOCTYPE HTML><html><body>");
+				out.println("	<link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css\""
+						+ " integrity=\"sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh\" crossorigin=\"anonymous\">");
+				out.println("	<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css\">");
+				out.println("<link rel=\"stylesheet\" href=\"search.css\">");
+
+				out.println("<p align = \"center\"> Pharmacy: " + pharmacy_id + "</p>\n");
+
+
+				//Table Headers
+				out.println("<table align =\"center\">");
+				out.println("<tr>");	
+				//out.println("<th>Date</th>");
+				out.println("<th>Drug Name</th>");
+				out.println("<th>Quantity Sold</th>");
+
+				//creates rows with data for each row from the result set
+				while(rs.next()) {
+					out.println("<tr>");
+					//out.println("<td>" + rs.getString("date") + "</td>");
+					out.println("<td align =\"left\">" + rs.getString("trade_name") + "</td>");
+					out.println("<td align =\"center\">" + rs.getString("sold") + "</td>");
+					out.println("</tr>");
+				}
+
+				rs.close();
+				out.println("</table>");
+				out.println("</body></html>");
+				conn.commit();
+
+
+			} catch (SQLException e) {
+				// Handle errors
+
+				e.printStackTrace();
+			}  
+		}
+		else {
 			out.println("<!DOCTYPE HTML><html><body>");
 			out.println("	<link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css\""
 					+ " integrity=\"sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh\" crossorigin=\"anonymous\">");
 			out.println("	<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css\">");
 			out.println("<link rel=\"stylesheet\" href=\"search.css\">");
-
-			out.println("<p> Pharmacy: " + pharmacy_id + "</p>\n");
-
-
-			//Table Headers
-			out.println("<table align =\"center\">");
-			out.println("<tr>");	
-			//out.println("<th>Date</th>");
-			out.println("<th>Drug Name</th>");
-			out.println("<th>Quantity Sold</th>");
-
-			//creates rows with data for each row from the result set
-			while(rs.next()) {
-				out.println("<tr>");
-				//out.println("<td>" + rs.getString("date") + "</td>");
-				out.println("<td align =\"left\">" + rs.getString("trade_name") + "</td>");
-				out.println("<td align =\"center\">" + rs.getString("sold") + "</td>");
-				out.println("</tr>");
-			}
-
-			rs.close();
-			out.println("</table>");
-			out.println("</body></html>");
-			conn.commit();
-
-
-		} catch (SQLException e) {
-			// Handle errors
-			e.printStackTrace();
-		}  
+			out.println("<p> Invalid Entry </p>");
+			
+		}
 	}
 
 }
